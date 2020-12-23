7e8f213fe1cd6b6b0d4bd75ebb1c076df9df85a8
mzelensky <max.m.zelensky@gmail.com>
2020-12-23 12:39:12 +0100

    use IndexedDB

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
 # Daily ToDo
 To Do list which is updated every day.
 
-### Instalation
-1. ```git clone https://github.com/MaxZell/Daily-ToDo.git```
-2. ```cd Daily-ToDo```
-3. ```yarn install``` or ```npm install```
-4. in index.js need to add [rapidapi](https://rapidapi.com/) key and host
-
-### Run
-1. ```node index.js```
-2. open [http://localhost:5000](http://localhost:5000)
---
+++ script/script.js
@@ -0,0 +1,71 @@
+﻿window.onload = loadToDos();
+
+//show todo add div
+function showToDoAdder(){
+    let toDoInputVisibility = document.getElementById('toDoInput').style.visibility;
+    if(toDoInputVisibility === "hidden"){
+        document.getElementById('toDoInput').style.visibility = "visible";
+        document.getElementById('todoList').style.visibility = "hidden";
+    }else{
+        document.getElementById('toDoInput').style.visibility = "hidden";
+        document.getElementById('todoList').style.visibility = "visible";
+    }
+}
+
+//send new ToDo Ticket as json to backend
+function saveTicket(){ 
+    let myTitle = document.getElementById('title').value;
+    let myDesc = document.getElementById('desc').value;
+    let newTicket = {id: "1", "title: ": myTitle, "desc: ": myDesc, isDone: false}
+
+    //open connection -> ToDo: open once
+    let dbExists = true;
+    let request = window.indexedDB.open('todo');
+    request.onupgradeneeded = function (e){
+        e.target.transaction.abort();
+        dbExists = false;
+    }
+    request.onerror = function(event) {
+        console.error(event.target.result);
+    };
+    request.onupgradeneeded = function(event) {
+        //add to db
+        var db = event.target.result;
+        var objectStore = db.createObjectStore("tickets");
+        objectStore.add(newTicket);
+        console.log("saved");
+        
+        //clear add form
+        showToDoAdder();
+        document.getElementById("title").value = '';
+        document.getElementById("desc").value = '';
+
+        //create new ticket div
+        var ticket = document.createElement("DIV");
+        ticket.innerHTML =`
+            <div class="ticket">
+                <label>${myTitle}</label></br><hr>
+                <label class="switch">
+                    <input type="checkbox" onclick="ticketIsDone()">
+                    <span class="slider round"></span>
+                </label>
+                <div class="desc">
+                    <span>
+                        ${myDesc}
+                    </span>  
+                </div>
+            </div>
+        `;
+        document.getElementById("todoList").appendChild(ticket); 
+    }
+} 
+
+//omLoad func: load ToDos Tickts from indexedDB
+function loadToDos(){
+    
+}
+
+//update ticket
+function ticketIsDone(){
+    console.log("done");
+}
