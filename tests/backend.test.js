3de6aa75e993d0a1986b4e62da72cbfecc0f44b2
mzelensky <max.m.zelensky@gmail.com>
2020-12-24 14:27:27 +0100

    to more get requests tests(only get 200 check yet)

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -4,6 +4,7 @@ const expect = chai.expect
 chai.use(chaiHttp)
 const app = require('../index.js')
 
+// serve index.js
 describe("Get /", () => {
 	it("should return status 200", async () => {
     	let res = await chai
@@ -12,3 +13,42 @@ describe("Get /", () => {
             expect(res.status).to.equal(200)
 	})
 })
+
+// get loadTickets
+describe("Get /loadTickets", () => {
+	it("should return status 200", async () => {
+    	let res = await chai
+        	.request(app)
+            .get('/loadTickets')
+            expect(res.status).to.equal(200)
+            //check DB
+	})
+})
+
+// get coronaApi
+describe("Get /coronaApi", () => {
+	it("should return status 200", async () => {
+    	let res = await chai
+        	.request(app)
+            .get('/coronaApi')
+            expect(res.status).to.equal(200)
+	})
+})
+
+// post insert
+// describe("Post /insert", () => {
+// 	it("should return status 200", async () => {
+//     	let res = await chai
+//         	.request(app)
+//             .post('/insert')
+//             .send({"title":"TEST", "description":"TEST"})
+//             expect('Content-Type', /json/)
+//             //check DB
+//             .end(function(err, res) {
+//                 if (err) done(err);
+//                 res.body.should.have.property('title');
+//                 res.body.participant.should.have.property('TEST');
+//             })
+//             .done();
+// 	})
+// })
